51468e9b13e4c6f3fb6a4d474becdd89eac5828a
Kershockr <tim.merino@live.com>
2022-05-01 01:49:04 -0400

    Various implementations of 3D gans, including WGAN

--- 3D-VAE/WGAN.py
+++ 3D-VAE/WGAN.py
@@ -4,26 +4,53 @@ import tensorflow
 from keras.layers import Input
 from keras.models import Model, Sequential
 from keras.layers import Convolution3D, MaxPooling3D, UpSampling3D, BatchNormalization, Dense, Flatten, Lambda, Reshape, Conv3DTranspose, LeakyReLU, Input, BatchNormalization, Activation, LeakyReLU
-from tensorflow.keras.optimizers import Adam
-from keras.regularizers import l2
-from keras.callbacks import TensorBoard
-from tensorflow.keras.optimizers import SGD
-from keras.activations import sigmoid
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import Adam, RMSprop
+from keras.constraints import Constraint
+from keras import backend
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
 import os
+import matplotlib.pyplot as plt
 
 K.clear_session()
 # K.set_image_dim_ordering('tf')
 # K.image_data_format() == 'channels_last'
 
+
+# from: https://machinelearningmastery.com/how-to-code-a-wasserstein-generative-adversarial-network-wgan-from-scratch/
+# implementation of wasserstein loss
+def wasserstein_loss(y_true, y_pred):
+    return backend.mean(y_true * y_pred)
+
+
+class ClipConstraint(Constraint):
+    # set clip value when initialized
+    def __init__(self, clip_value):
+        self.clip_value = clip_value
+
+    # clip model weights to hypercube
+    def __call__(self, weights):
+        return backend.clip(weights, -self.clip_value, self.clip_value)
+
+    # get the config
+    def get_config(self):
+        return {'clip_value': self.clip_value}
+
+
+def plot_history(d1_hist, d2_hist, g_hist, out_dir, model_name):
+    # plot history
+    plt.plot(d1_hist, label='crit_real')
+    plt.plot(d2_hist, label='crit_fake')
+    plt.plot(g_hist, label='gen')
+    plt.legend()
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    plt.savefig(out_dir + '/' + model_name + '_loss.png')
+    plt.close()
+
 # from https://github.com/enochkan/3dgan-keras/blob/master/models.py
 # paper http://3dgan.csail.mit.edu/papers/3dgan_nips.pdf
-
 # =====================
 #   GENERATOR
 # =====================
@@ -56,24 +83,28 @@ def get_generator(latent_dim=100, outdim=32, outchannels=15, kernel_size=(4,4,4)
 # =====================
 #   DISCRIMINATOR
 # =====================
-def get_discriminator(latent_dim=100, outdim=32, outchannels=15, kernel_size=(4,4,4), strides=(2,2,2)):
-
+def get_critic(latent_dim=100, outdim=32, outchannels=15, kernel_size=(4,4,4), strides=(2,2,2)):
+    # weight constraint
+    initializer = tf.keras.initializers.RandomNormal(mean=0., stddev=1.)
+    const = ClipConstraint(.1)
     model = Sequential()
     # model.add(Convolution3D(filters=64, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer='zeros', padding='same'))
     # model.add(BatchNormalization())
     # model.add(LeakyReLU(alpha=0.2))
-    model.add(Convolution3D(filters=128, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer='zeros', padding='same'))
+    model.add(Convolution3D(filters=128, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer=initializer, padding='same', kernel_constraint=const))
     model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Convolution3D(filters=256, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer='zeros', padding='same'))
+    model.add(Convolution3D(filters=256, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer=initializer, padding='same', kernel_constraint=const))
     model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Convolution3D(filters=512, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer='zeros', padding='same'))
+    model.add(Convolution3D(filters=512, kernel_size=kernel_size, strides=strides, kernel_initializer='glorot_normal', bias_initializer=initializer, padding='same', kernel_constraint=const))
     model.add(BatchNormalization())
     model.add(LeakyReLU(alpha=0.2))
-    model.add(Convolution3D(filters=1, kernel_size=kernel_size, strides=(1, 1, 1), kernel_initializer='glorot_normal', bias_initializer='zeros', padding='valid'))
+    model.add(Convolution3D(filters=1, kernel_size=kernel_size, strides=(1, 1, 1), kernel_initializer='glorot_normal', bias_initializer=initializer, padding='valid', kernel_constraint=const))
     model.add(BatchNormalization())
-    model.add(Activation('sigmoid'))
+    # model.add(Activation('sigmoid'))
+    # WGAN uses linear activation as critic output
+    model.add(Dense(1, activation='linear'))
     #
     image = Input(shape=(outdim, outdim, outdim, outchannels))
     # model.summary()
@@ -85,21 +116,25 @@ def get_discriminator(latent_dim=100, outdim=32, outchannels=15, kernel_size=(4,
 
 
 
+
 # d_lr = 1e-5
-d_lr = .003
-g_lr = .003
-b1 = .5
-batch_size = 512
-epochs = 5000
+# d_lr = .003
+# g_lr = .003
+# b1 = .5
+# lr=0.00005
+lr=0.02
+batch_size = 128
+epochs = 100
 latent_dim = 100
 sample_epoch = 1000
 save_epoch = 1000
-model_name = 'model_5'
+model_name = 'WGAN_model_pointoh2LR'
 sample_path = "GAN_generated_samples/" + model_name + "/"
 checkpoints_path = "GAN_models/" + model_name + "/"
 
-dis_optim = Adam(lr=d_lr, beta_1=b1)
-gen_optim = Adam(lr=g_lr, beta_1=b1)
+# dis_optim = Adam(lr=d_lr, beta_1=b1)
+# gen_optim = Adam(lr=g_lr, beta_1=b1)
+opt = RMSprop(lr=lr)
 
 generator = get_generator()
 
@@ -108,17 +143,17 @@ generator.summary()
 z = Input(shape=(1, 1, 1, latent_dim))
 img = generator(z)
 
-discriminator = get_discriminator()
+critic = get_critic()
 print('Discriminator...')
-discriminator.summary()
-discriminator.compile(loss='binary_crossentropy', optimizer=dis_optim)
+critic.summary()
+critic.compile(loss=wasserstein_loss, optimizer=opt)
 
 # make discriminator not trainable
-discriminator.trainable = False
-validity = discriminator(img)
+critic.trainable = False
+validity = critic(img)
 
 combined = Model(z, validity)
-combined.compile(loss='binary_crossentropy', optimizer=gen_optim)
+combined.compile(loss=wasserstein_loss, optimizer=opt)
 
 train = np.load('../house_combined_numpy_file/compressedcategorical_combined_rotated_flipped.npy')
 # print(train.shape)
@@ -137,38 +172,49 @@ train = tf.one_hot(train, 15, dtype=tf.int8).numpy()
 # print(enc.categories_)
 # print(le.classes_)
 dl, gl = [],[]
-
+dl_r, dl_f = [], []
+# number of times we train the critic for every time we train the gen
+n_critic = 5
 for epoch in range(epochs):
-    # print("Epoch " + str(epoch) + "...")
-    #sample a random batch
-    idx = np.random.randint(len(train), size=batch_size)
-    real = train[idx]
-    # print(len(train))
+    dl_r_temp, dl_f_temp, dl_tot_temp = [], [], []
+    for _ in range(n_critic):
+        print("training critic...")
+        #sample a random batch
+        idx = np.random.randint(len(train), size=batch_size)
+        real = train[idx]
 
-    z = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, latent_dim]).astype(np.float32)
-    fake = generator.predict(z)
+        # create fake samples
+        z = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, latent_dim]).astype(np.float32)
+        fake = generator.predict(z)
 
-    # print(real.shape)
-    # real = np.expand_dims(real, axis=4)
-    # print(real.shape)
+        # Create labels for reals and fakes. WGAN uses -1 or 1 labels
+        lab_real = np.reshape([1.0] * batch_size, (-1, 1, 1, 1, 1))
+        lab_fake = np.reshape([-1.0] * batch_size, (-1, 1, 1, 1, 1))
 
-    lab_real = np.reshape([1] * batch_size, (-1, 1, 1, 1, 1))
-    lab_fake = np.reshape([0] * batch_size, (-1, 1, 1, 1, 1))
-    # print(lab_real.shape)
+        # train critic, calculate critic loss on reals and fakes
+        d_loss_real_temp = critic.train_on_batch(real, lab_real)
+        d_loss_fake_temp = critic.train_on_batch(fake, lab_fake)
+        dl_r_temp.append(d_loss_real_temp)
+        dl_f_temp.append(d_loss_fake_temp)
 
-    # calculate discrminator loss
-    d_loss_real = discriminator.train_on_batch(real, lab_real)
-    d_loss_fake = discriminator.train_on_batch(fake, lab_fake)
-
-    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        d_loss_tot_temp = 0.5 * np.add(d_loss_real_temp, d_loss_fake_temp)
+        dl_tot_temp.append(d_loss_tot_temp)
 
     z = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, latent_dim])#.astype(np.float32)
+    print("train generator...")
+    # calculate generator and critic loss
+    g_loss = combined.train_on_batch(z, np.reshape([1.0] * batch_size, (-1, 1, 1, 1, 1)))#.astype(np.float64)
+    d_loss = sum(dl_tot_temp) / len(dl_tot_temp)
+    d_loss_real = sum(dl_r_temp) / len(dl_r_temp)
+    d_loss_fake = sum(dl_f_temp) / len(dl_f_temp)
 
-    # calculate generator loss
-    g_loss = combined.train_on_batch(z, np.reshape([1] * batch_size, (-1, 1, 1, 1, 1)))#.astype(np.float64)
-
+    # append losts to list to track convergence
+    dl_r.append(d_loss_real)
+    dl_f.append(d_loss_fake)
     dl.append(d_loss)
     gl.append(g_loss)
+
+    # calculate average critic and generator losses
     avg_d_loss = round(sum(dl) / len(dl), 4)
     avg_g_loss = round(sum(gl) / len(gl), 4)
 
@@ -188,13 +234,17 @@ for epoch in range(epochs):
         if not os.path.exists(checkpoints_path):
             os.makedirs(checkpoints_path)
         generator.save_weights(checkpoints_path + '/generator_epoch_' + str(epoch + 1), True)
-        discriminator.save_weights(checkpoints_path + '/discriminator_epoch_' + str(epoch + 1), True)
+        critic.save_weights(checkpoints_path + '/critic_epoch_' + str(epoch + 1), True)
+
 
 print("Training finished....")
+print("Plotting...")
+# plot a graph of our critics loss on reals and fakes, and generator loss
+plot_history(dl_r, dl_f, gl, "GAN_models/" + model_name + "/plots", model_name)
 print('Sampling...')
 sample_noise = np.random.normal(0, 0.33, size=[batch_size, 1, 1, 1, latent_dim]).astype(np.float32)
 generated_volumes = generator.predict(sample_noise, verbose=1)
 generated_volumes.dump(sample_path + '/sample_final.npy')
 
 generator.save_weights(checkpoints_path + '/generator_final', True)
-discriminator.save_weights(checkpoints_path + '/discriminator_final', True)
+critic.save_weights(checkpoints_path + '/critic_final', True)
---
+++ 3D-VAE/processGANsamples.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import numpy as np
+from nbtschematic import SchematicFile
+import os
+
+compression_list = [[0, 6, 26, 27, 28, 30, 55, 63, 65, 66, 68, 69, 70, 72, 77, 97, 104, 117, 127, 131, 132, 143, 144, 147, 148, 149, 167, 171, 50, 51, 76, 105, 123],[1, 4, 7, 29, 33, 34, 46, 48, 49, 52, 54, 61, 87, 89, 98, 45, 112, 120, 121, 139, 155, 158, 168, 169, 201, 202, 206, 215, 216, 218, 219, 220, 221, 222, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 35, 14, 15, 16, 21, 56, 73, 129, 153],[2, 3, 82, 88, 159, 172, 173, 214],[5, 17, 25, 47, 58, 84, 96, 116, 130, 140, 146, 151, 154, 162],[8, 9, 10, 11, 213],[12, 13, 19, 24, 179],[18, 31, 32, 81, 86, 91, 103, 106, 161, 170, 199, 200, 207],[20, 92, 102, 160, 95],[22, 23, 41, 42, 57, 118, 133, 138, 145, 152, 165],[37, 38, 39, 40, 59, 83, 110, 115, 141, 142, 175],[43, 44, 92, 125, 126, 181, 182, 204, 205],[53, 67, 108, 109, 114, 128, 134, 135, 136, 156, 163, 164, 180, 203],[64, 71, 193, 194, 195, 196, 197],[78, 79, 80, 174],[85, 101, 107, 113, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 198],]
+
+def decode_gan_samples(file):
+    data = np.load(file, allow_pickle=True)
+    data = tf.argmax(data, axis=4).numpy()
+    return data
+
+def decompress(data):
+    for i in range(len(compression_list)):
+        data[data == i] = compression_list[i][0]
+    return data
+
+def convert_to_schem(data, out_dir):
+    # print(len(data))
+    for i in range(len(data)):
+
+        build = data[i]
+        # print(build.shape)
+        arr_axes = (build.shape[0], build.shape[1], build.shape[2])
+        sf = SchematicFile(shape=arr_axes)
+        assert sf.blocks.shape == arr_axes
+        for index, block_id in np.ndenumerate(build):
+            sf.blocks[index[0], index[1], index[2]] = block_id
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        sf.save(out_dir + "/generated_" + str(i) + ".schematic")
+
+def process_binary_samples(sample_dir, out_dir):
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    for file in os.listdir(sample_dir):
+        sample = np.load(sample_dir + file)
+        print(sample.shape)
+        file_name = os.path.splitext(file)[0]
+        arr_axes = (sample.shape[0], sample.shape[1], sample.shape[2])
+        sf = SchematicFile(shape=arr_axes)
+        assert sf.blocks.shape == arr_axes
+        for index, block_id in np.ndenumerate(sample):
+            sf.blocks[index[0], index[1], index[2]] = block_id
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        sf.save(out_dir + "/" + file_name + ".schematic")
+
+
+# =========== One-hot encoded code ===========
+categorical = decode_gan_samples('GAN_generated_samples/WGAN_model_highLR/sample_final.npy')
+decompressed = decompress(categorical)
+uniques, counts = np.unique(decompressed, return_counts=True)
+print(print(dict(zip(np.array(uniques), counts))))
+convert_to_schem(decompressed, 'GAN_generated_samples/WGAN_model_highLR/schematics_final')
+
+# ============= binary code ===========
+# process_binary_samples('GAN_generated_samples/binaryGAN_test2/', 'GAN_generated_samples/binaryGAN_test2/schematics')
